5e5165956511bd3730425422256f997d6b41a581
Luke Epp <lucasfepp@gmail.com>
2016-05-20 17:01:43 -0700

    Added todos

--- REST_API_v2/Incidents/snooze_incident.py
+++ REST_API_v2/Incidents/snooze_incident.py
@@ -24,6 +24,9 @@
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# TODO: Remove requester_id when deprecated
+# TODO: Test after internal server error is resolved
 
 import requests
 import json
